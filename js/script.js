f2787058d9e53b8bc855fd09963d78e616dd8669
RoshanTanisha <t.bhayani@yahoo.com>
2019-09-10 09:11:53 +0530

    completed about me

--- js/script.js
+++ js/script.js
@@ -2,54 +2,56 @@
 
 console.log("script script.js loaded...");
 
-function load_content(what_to_load){
-    var content_link = null;
-    var tag_name = null;
-    var content_tag_name = null;
-    if (what_to_load === "blog_content") {
-        content_link = "blog_list.xml";
-        tag_name = "posts";
-        content_tag_name = "content";
-    }
-    // console.log("content_link = ", content_link);
-    // console.log("tag_name = ", tag_name);
-    // console.log("content_tag_name = ", content_tag_name);
-    xhr = new XMLHttpRequest();
+(document.onready= function () {
+    console.log('document loaded');
+    short_content_url = "blog_list.xml";
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', short_content_url);
+
     xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            load_posts(this);
+        if (xhr.status === 200) {
+            xml_document = xhr.responseXML;
+            handle_xml_document(xml_document);
         }
-    }
-    xhr.open("GET", content_link, true);
+    };
+
     xhr.send();
-    function load_posts() {
-        xmlDoc = xhr.responseXML;
-        post = xmlDoc.getElementsByTagName(tag_name);
-        content = document.getElementById(content_tag_name);
-        var string = "", tag, link;
-        for (i = 0; i < post.length; i++) {
-            for (j = 0; j < post[i].children.length; j++) {
-                tag = post[i].children[j].nodeName;
-                console.log("TAG :: ", tag);
-                if (tag === "title") {
-                    try {
-                        link = post[i].children[j].nextElementSibling;
-                        // console.log("$$", link.innerHTML);
-                    } catch (e) {
-                        console.log(e);
-                    }
-                    // console.log("String::")
-                    string += "<a href = \"" + link.innerHTML + "\"><h3>" + post[i].children[j].innerHTML + "</h3></a><br>";
-                    // console.log(string);
-                    j++;
-                } else if (tag === "short_text") {
-                    string += "<i>" + post[i].children[j].innerHTML + "</i><hr>";
-                } else {
-                    console.log(post[i].children[j].innerHTML);
-                    string += "<u>" + post[i].children[j].innerHTML + "</u>&nbsp;&nbsp;";
-                }
-            }
-        }
-        content.innerHTML += string;
+    console.log('sending xhr request');
+}());
+
+function handle_xml_document(xml_document) {
+    if (xml_document === null) {
+        return;
     }
+    console.log('in handle_xml_document function');
+    let POST_PER_ROW = 3;
+    console.log('xml_document = ', xml_document);
+    $xml = $(xml_document);
+    $index = 1;
+    $appended = false;
+    $appendChildString = '';
+    console.log($xml.find('posts'));
+    $xml.find('posts').each(function() {
+        $appended = false;
+        $link = $(this).find("link").text().trim();
+        $title = '<a href="' + $link + '"><h5 class="card-title text-xl-center">' + $(this).find("title").text().trim() + '</h5></a>';
+        $datetime = '<small class="card-subtitle">' + $(this).find("date_time").text().trim() + '</small>';
+        $blog_image = '<img alt="' + $(this).find("short_text").text().trim() +'" src="' +
+        $(this).find("blog_image").text().trim() + '" class="card-img">';
+        $shortText = '<div class="card-body">' + $(this).find("short_text").text().trim() + '</div>';
+        $appendChildString = $appendChildString + '<div class="card col-md-4 rounded">' + $title + $datetime + $blog_image + $shortText + '</div>';
+        if ($index % POST_PER_ROW === 0) {
+            $appendChildString = '<div class="card-deck d-flex row">' + $appendChildString + '</div><br/>';
+            $("#content").append($appendChildString);
+            $appended = true;
+            $appendChildString = '';
+        }
+        $index = $index + 1;
+        console.log($index);
+    });
+    if (!$appended) {
+        $("#content").append('<div class="card-deck row">' + $appendChildString + '</div>');
+    }
+    console.log(document.getElementById("content"));
 }
