f25481e551e63e354f69934d0f84a9d1cf3fb2ac
RoshanTanisha <t.bhayani@yahoo.com>
2019-09-14 11:03:43 +0530

    added projects

---
+++ js/load_projects.js
@@ -0,0 +1,34 @@
+console.log('load_talk.js loaded...');
+
+(document.onready = function() {
+
+    console.log('document.onready');
+
+    let xml_document = 'xmls/projects.xml';
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', xml_document);
+    xhr.onreadystatechange = function(){
+        console.log(xhr.status);
+        if (xhr.status === 200) {
+            let xmlString = xhr.responseXML;
+            console.log(xmlString);
+            handle_xml_document(xmlString);
+        }
+    };
+    xhr.send();
+}());
+
+
+function handle_xml_document(xmlString) {
+    if (xmlString === null) {
+        return;
+    }else {
+        $xml = $(xmlString);
+        $xml.find('project').each(function(){
+            $title = '<div class="popover-header">' + $(this).find("title").text().trim() + '</div>';
+            $description = '<div class="popover-body text-justify">' + $(this).find("description").text().trim() + '</div>';
+            $tech_stack = '<div class="popover-body"> Tech Stack: ' + $(this).find("tech_stack").text().trim() + '</div>';
+            $("#content").append('<div class="container container-fluid">' + $title + $description + $tech_stack + '</div>');
+        });
+    }
+}
